07848492252ce6487a1b35b60b884eb7bcc08ef7
JosefTroe <68950116+JosefTroe@users.noreply.github.com>
2021-05-28 10:05:14 +0200

    Create rss.py

---
+++ rss.py
@@ -0,0 +1,22 @@
+import feedparser
+
+#The personal key of the moodle User has (for now) be added to the RSS-URL (insert yours for the URL to work)
+personal_key= '51d29206b6edd887f14de1c177fb3bc1'
+
+feed = feedparser.parse(f'https://isis.tu-berlin.de/rss/file.php/1970720/{personal_key}/mod_forum/64807/rss.xml')
+
+entries = feed['entries']
+
+def refreshFeed():
+    new_feed = feedparser.parse(f'https://isis.tu-berlin.de/rss/file.php/1970720/{personal_key}/mod_forum/64807/rss.xml')
+    new_entries = new_feed['entries']
+    curr_entries = new_entries
+
+    for x in new_entries:
+        for y in entries:
+            if(x['id'] == y['id']):
+                new_entries.remove(x)
+
+
+
+refreshFeed()
