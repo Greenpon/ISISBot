02d4cfc576204e3834bce9d59b0fb608a57baf9d
lennartstoelting <l.stoelting@campus.tu-berlin.de>
2021-06-25 16:04:03 +0200

    changed all d2 variables in ShowForum to bot.get_date() as addition to my last push

--- ShowForum.py
+++ ShowForum.py
@@ -6,6 +6,7 @@ from discord.ext import commands
 from datetime import date
 from io import StringIO
 from html.parser import HTMLParser
+import bot
 
 import config
 import rss
@@ -13,8 +14,6 @@ import Filtering
 
 # DATE DEF
 today = date.today()
-d1 = today.strftime("%d/%m")
-d2 = today.strftime("%d/%m/%y")
 
 #Key-Value Pairs for the Feed, so the user can set multiple Feeds at once
 #Structure {Course.id : Forum.id}
@@ -89,7 +88,7 @@ class ShowForum(commands.Cog):
                                     noti.add_field(name=entry_in_feed.title + " " + author,
                                                    value="```" + message + "```" + "\n" + entry_in_feed.published + "\n" + "[Direktlink]({})".format(
                                                        entry_in_feed.link), inline=False)
-                                    noti.set_footer(text="ISIS Bot v0.1 • " + d2,
+                                    noti.set_footer(text="ISIS Bot v0.1 • " + bot.get_date(),
                                                     icon_url="https://i.imgur.com/s8Ni2X1.png")
 
                                     await ctx.send(embed=noti)
@@ -120,7 +119,7 @@ class ShowForum(commands.Cog):
                                                 noti.add_field(name=entry_in_feed.title + " " + author,
                                                                value="```yaml\n**" + message + "**```" + "\n" + entry_in_feed.published + "\n" + "[Direktlink]({})".format(
                                                                    entry_in_feed.link), inline=False)
-                                                noti.set_footer(text="ISIS Bot v0.1 • " + d2,
+                                                noti.set_footer(text="ISIS Bot v0.1 • " + bot.get_date(),
                                                                 icon_url="https://i.imgur.com/s8Ni2X1.png")
 
                                                 await ctx.send(embed=noti)
@@ -132,7 +131,7 @@ class ShowForum(commands.Cog):
                                                 noti.add_field(name=entry_in_feed.title + " " + author,
                                                                value="```" + message + "```" + "\n" + entry_in_feed.published + "\n" + "[Direktlink]({})".format(
                                                                    entry_in_feed.link), inline=False)
-                                                noti.set_footer(text="ISIS Bot v0.1 • " + d2,
+                                                noti.set_footer(text="ISIS Bot v0.1 • " + bot.get_date(),
                                                                 icon_url="https://i.imgur.com/s8Ni2X1.png")
 
                                                 await ctx.send(embed=noti)
@@ -163,7 +162,7 @@ class ShowForum(commands.Cog):
                 success = discord.Embed(title="", color=0x990000)
                 success.set_thumbnail(url="https://i.imgur.com/TBr8R7L.png")
                 success.add_field(name="New Feed", value="You did set up a new RSS Feed!", inline=False)
-                success.set_footer(text="ISIS Bot v0.1 • " + d2, icon_url="https://i.imgur.com/s8Ni2X1.png")
+                success.set_footer(text="ISIS Bot v0.1 • " + bot.get_date(), icon_url="https://i.imgur.com/s8Ni2X1.png")
 
                 await ctx.send(embed=success, delete_after=10.0)
 
@@ -173,7 +172,7 @@ class ShowForum(commands.Cog):
                 warn = discord.Embed(title="", color=0x990000)
                 warn.set_thumbnail(url="https://i.imgur.com/TBr8R7L.png")
                 warn.add_field(name="Wrong Input", value=e, inline=False)
-                warn.set_footer(text="ISIS Bot v0.1 • " + d2, icon_url="https://i.imgur.com/s8Ni2X1.png")
+                warn.set_footer(text="ISIS Bot v0.1 • " + bot.get_date(), icon_url="https://i.imgur.com/s8Ni2X1.png")
 
                 await ctx.send(embed=warn, delete_after=10.0)
 
@@ -200,7 +199,7 @@ class ShowForum(commands.Cog):
                 success = discord.Embed(title="", color=0x990000)
                 success.set_thumbnail(url="https://i.imgur.com/TBr8R7L.png")
                 success.add_field(name="Removed Feed", value="You did remove a RSS Feed!", inline=False)
-                success.set_footer(text="ISIS Bot v0.1 • " + d2, icon_url="https://i.imgur.com/s8Ni2X1.png")
+                success.set_footer(text="ISIS Bot v0.1 • " + bot.get_date(), icon_url="https://i.imgur.com/s8Ni2X1.png")
 
                 await ctx.send(embed=success, delete_after=10.0)
 
@@ -208,7 +207,7 @@ class ShowForum(commands.Cog):
                 warn = discord.Embed(title="", color=0x990000)
                 warn.set_thumbnail(url="https://i.imgur.com/TBr8R7L.png")
                 warn.add_field(name="Wrong Input", value=e, inline=False)
-                warn.set_footer(text="ISIS Bot v0.1 • " + d2, icon_url="https://i.imgur.com/s8Ni2X1.png")
+                warn.set_footer(text="ISIS Bot v0.1 • " + bot.get_date(), icon_url="https://i.imgur.com/s8Ni2X1.png")
 
                 await ctx.send(embed=warn, delete_after=10.0)
 
@@ -258,14 +257,14 @@ class ShowForum(commands.Cog):
                     changeSuccess.set_thumbnail(url="https://i.imgur.com/TBr8R7L.png")
                     changeSuccess.add_field(
                         name="Warning", value="You have set your bot to check for forum entries less than three times a day. Due to restrictions by moodle and the RSS feed, Isi can only recieve the latest ten entries. \n \n If the forum Isi is listening to has a lot of traffic, you might miss out on some of the entries.", inline=False)
-                changeSuccess.set_footer(text="ISIS Bot v0.1 • " + d2, icon_url="https://i.imgur.com/s8Ni2X1.png")
+                changeSuccess.set_footer(text="ISIS Bot v0.1 • " + bot.get_date(), icon_url="https://i.imgur.com/s8Ni2X1.png")
                 await ctx.send(embed=changeSuccess)
 
             except ValueError as e:
                 warn = discord.Embed(title="", color=0x990000)
                 warn.set_thumbnail(url="https://i.imgur.com/TBr8R7L.png")
                 warn.add_field(name="Wrong Input", value=e, inline=False)
-                warn.set_footer(text="ISIS Bot v0.1 • " + d2, icon_url="https://i.imgur.com/s8Ni2X1.png")
+                warn.set_footer(text="ISIS Bot v0.1 • " + bot.get_date(), icon_url="https://i.imgur.com/s8Ni2X1.png")
 
                 await ctx.send(embed=warn, delete_after=10.0)
 
